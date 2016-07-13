479ac611db1923a8f00bfb05c70faecbbedad796
nursaruhan <nur.saruhan@bil.omu.edu.tr>
2016-07-13 13:53:11 +0300

    item modeli olusturuldu ve baglantısı saglandı

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713085028) do
+ActiveRecord::Schema.define(version: 20160713103650) do
+
+  create_table "items", force: :cascade do |t|
+    t.text     "content",      limit: 65535
+    t.boolean  "is_completed"
+    t.integer  "user_id",      limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
